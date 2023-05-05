ee8d07a8ba24dcf5026c09273a641b14dd521b5c
shrugs <mattgcondon@gmail.com>
2023-05-04 21:18:46 -0400

    fix: 404 correctly, better lg+ layout

--- src/routes/[tokenId]/+page.server.ts
+++ src/routes/[tokenId]/+page.server.ts
@@ -1,5 +1,6 @@
 import { ALEATORIC_ADDRESS } from '$lib/constants';
 import { zdk } from '$lib/zdk';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const prerender = 'auto';
@@ -10,6 +11,8 @@ export const load = (async ({ params }) => {
   const result = await zdk.token({
     token: { address: ALEATORIC_ADDRESS, tokenId: params.tokenId }
   });
+
+  if (!result.token) throw error(404);
 
   return {
     token: result.token,
--- src/routes/[tokenId]/+page.svelte
+++ src/routes/[tokenId]/+page.svelte
@@ -19,20 +19,20 @@
   <div class="flex flex-row justify-center">
     <a class="text-xs px-4 py-4 text-gray-500" href="/">home</a>
   </div>
-  <div class="min-w-0 flex-1 flex flex-col md:flex-row items-center gap-2 md:gap-4">
+  <div class="min-w-0 flex-1 flex flex-col lg:flex-row items-center gap-2 lg:gap-4">
     <a href="/{prevTokenId}">
       <div class="p-8 flex justify-center">
-        <span class="md:hidden"><img class="h-4 w-4" src="/arrow-up.svg" alt="up arrow" /></span>
-        <span class="hidden md:block"
+        <span class="lg:hidden"><img class="h-4 w-4" src="/arrow-up.svg" alt="up arrow" /></span>
+        <span class="hidden lg:block"
           ><img class="h-4 w-4" src="/arrow-left.svg" alt="left arrow" /></span
         >
       </div></a
     >
-    <div class="flex-1 flex flex-col md:flex-row justify-center w-full gap-4">
-      <div class="flex-1 flex flex-col">
+    <div class="flex-1 flex flex-col lg:flex-row justify-center w-full gap-8">
+      <div class="flex-1 flex flex-col justify-center">
         <AleatoricVideo {uri} controls />
       </div>
-      <div class="flex flex-col gap-2 md:max-w-xl p-4 md:p-0">
+      <div class="flex flex-col gap-2 lg:w-96 p-4 lg:p-0">
         <div class="flex-1 flex-col gap-2">
           <h1 class="text-xl">{name}</h1>
           {#if description}
@@ -52,9 +52,9 @@
     </div>
     <a href="/{nextTokenId}">
       <div class="p-8 flex justify-center">
-        <span class="md:hidden"><img class="h-4 w-4" src="/arrow-down.svg" alt="down arrow" /></span
+        <span class="lg:hidden"><img class="h-4 w-4" src="/arrow-down.svg" alt="down arrow" /></span
         >
-        <span class="hidden md:block"
+        <span class="hidden lg:block"
           ><img class="h-4 w-4" src="/arrow-right.svg" alt="right arrow" /></span
         >
       </div>
